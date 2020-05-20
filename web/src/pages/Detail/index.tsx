b3aad92dfcc6f20e267a2ebc3ce9b05fa0e987f4
André Victor <andrevictor50@gmail.com>
2020-05-20 07:42:06 -0300

    🚀: prepared frontend web configs

---
+++ web/src/pages/Detail/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Detail: React.FC = () => {
+  return <div>Detail</div>;
+};
+
+export default Detail;
---
+++ web/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Detail from '../pages/Detail';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/detail" component={Detail} />
+  </Switch>
+);
+
+export default Routes;
